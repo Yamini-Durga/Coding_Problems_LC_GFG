5318150307688026523ee449ccd14985e628aaa8
Durga yamini <101850697+Yamini-Durga@users.noreply.github.com>
2022-12-16 09:09:11 +0530

    Update ReverseLinkedList.cpp

--- Linked List/ReverseLinkedList.cpp
+++ Linked List/ReverseLinkedList.cpp
@@ -2,12 +2,20 @@ https://leetcode.com/problems/reverse-linked-list/description/
 
 ListNode* reverseList(ListNode* head) {
         // iterative approach
-        ListNode *prev = NULL, *cur = head, *nextNode = NULL;
-        while(cur != NULL){
-            nextNode = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = nextNode;
-        }
-        return prev;
+        // ListNode *prev = NULL, *cur = head, *nextNode = NULL;
+        // while(cur != NULL){
+        //     nextNode = cur->next;
+        //     cur->next = prev;
+        //     prev = cur;
+        //     cur = nextNode;
+        // }
+        // return prev;
+
+        // recursive approach
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode *newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
     }
