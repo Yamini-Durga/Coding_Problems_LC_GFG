e4beee83d3f36b9ebc2102aff6067f9551fc800a
Durga yamini <101850697+Yamini-Durga@users.noreply.github.com>
2022-12-17 17:33:28 +0530

    Create MiddleOfTheLinkedlist.cpp

---
+++ Linked List/MiddleOfTheLinkedlist.cpp
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
