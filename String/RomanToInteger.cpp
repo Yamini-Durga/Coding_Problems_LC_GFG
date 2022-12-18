c70119127aaba9c75e525c3fad99673477d09398
Durga yamini <101850697+Yamini-Durga@users.noreply.github.com>
2022-12-18 11:56:13 +0530

    Update RomanToInteger.cpp

--- String/RomanToInteger.cpp
+++ String/RomanToInteger.cpp
@@ -20,3 +20,44 @@ int romanToInt(string s) {
         }
         return res;
     }
+// without extra space
+int romanToInt(string s) {
+        int res = 0;
+        for(int i=0; i<s.length(); i++){
+            switch(s[i]){
+                case 'I':
+                    if((i+1)<s.length() && (s[i+1]=='V' || s[i+1]=='X'))
+                        res -= 1;
+                    else
+                        res += 1;
+                    break;
+                case 'V':
+                    res += 5;
+                    break;
+                case 'X':
+                    if((i+1)<s.length() && (s[i+1]=='L' || s[i+1]=='C'))
+                        res -= 10;
+                    else
+                        res += 10;
+                    break;
+                case 'L':
+                    res += 50;
+                    break;
+                case 'C':
+                    if((i+1)<s.length() && (s[i+1]=='D' || s[i+1]=='M'))
+                        res -= 100;
+                    else
+                        res += 100;
+                    break;
+                case 'D':
+                    res += 500;
+                    break;
+                case 'M':
+                    res += 1000;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return res;
+    }
