df649756d5ab88e201714da4d4bd8406c1076abe
Durga yamini <101850697+Yamini-Durga@users.noreply.github.com>
2023-03-06 12:39:22 +0530

    Update IntesectionofLLs.cpp

--- Linked List/IntesectionofLLs.cpp
+++ Linked List/IntesectionofLLs.cpp
@@ -39,3 +39,39 @@ public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
 
 // T(n) = O(m+n) S(n) = O(1)
 // C++ code 
+// using difference of length
+int lengthDiff(ListNode *headA, ListNode *headB)
+    {
+        ListNode *tempA = headA, *tempB = headB;
+        int lenA = 0, lenB = 0;
+        while(tempA != NULL || tempB != NULL)
+        {
+            if(tempA != NULL){
+                lenA++;
+                tempA = tempA->next;
+            }
+            if(tempB != NULL){
+                lenB++;
+                tempB = tempB->next;
+            }
+        }
+        return lenA-lenB;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // Diff = +ve --> List A is larger ow List B
+        int diff = lengthDiff(headA, headB);
+        if(diff > 0){
+            while(diff-- != 0)
+                headA = headA->next;
+        }
+        else {
+            while(diff++ != 0)
+                headB = headB->next;
+        }
+        while(headA != headB)
+        {
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return headA;
+    }
